524a6017f2b236dbe679f3b44552f6f06204b56d
Raul Duenez <41699768+1digitalnomad@users.noreply.github.com>
2019-01-17 21:32:18 -0800

    In progress.

--- 01-javascript/02-js-fundamentals/01-js-objects.js
+++ 01-javascript/02-js-fundamentals/01-js-objects.js
@@ -1,1 +1,38 @@
 // 30 minutes max
+
+// let students = [
+//     { name: 'Remy', cohort: 'Jan' },
+//     { name: 'Genevieve', cohort: 'March' },
+//     { name: 'Chuck', cohort: 'Jan' },
+//     { name: 'Osmund', cohort: 'June' },
+//     { name: 'Nikki', cohort: 'June' },
+//     { name: 'Boris', cohort: 'June' }
+// ];
+
+// for (let key in students) {
+//     console.log("Name:", students[key].name, ",", "Cohort", students[key].cohort)
+// }
+
+
+let users = {
+    employees: [
+        { 'first_name': 'Miguel', 'last_name': 'Jones' },
+        { 'first_name': 'Ernie', 'last_name': 'Bertson' },
+        { 'first_name': 'Nora', 'last_name': 'Lu' },
+        { 'first_name': 'Sally', 'last_name': 'Barkyoumb' }
+    ],
+    managers: [
+        { 'first_name': 'Lillian', 'last_name': 'Chambers' },
+        { 'first_name': 'Gordon', 'last_name': 'Poe' }
+    ]
+};
+
+//still working on this one.
+for (let user in users){{
+    console.log(user);
+    
+}
+    for (var element in employees);{
+    console.log(element);
+    }
+} 
