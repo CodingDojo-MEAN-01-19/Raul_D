1ac851a5f085baa3a5b7b941351978cb26532cb2
Raul Duenez <41699768+1digitalnomad@users.noreply.github.com>
2019-01-24 20:22:18 -0800

    ninja III assignment.

--- 01-javascript/03-js-oop/03-ninja-class-iii.js
+++ 01-javascript/03-js-oop/03-ninja-class-iii.js
@@ -28,3 +28,27 @@ const ninja1 = new Ninja("Harry", 4)
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+class Sensei extends Ninja{
+    constructor(name, health = 200, speed=10, strength=10, wisdom=10) {
+        
+        super(name, health, speed, strength);
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+        this.wisdom = wisdom;
+    }
+    childFunction(){
+        const messageSake = super.drinkSake();
+    }
+    speakWisdom(){
+        const wize = super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
