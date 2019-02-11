8fa75dbba4c240d0fae0d8fe7f850122299436ad
Raul <Takethefirststep@Rauls-iMac.attlocal.net>
2019-02-11 01:00:27 -0800

    updated js file

--- 01-javascript/02-js-fundamentals/github/js/github.js
+++ 01-javascript/02-js-fundamentals/github/js/github.js
@@ -1,7 +1,16 @@
 $("button").click(function () {
-    $.get("https://api.github.com/users/1digitalnomad", displayName)
-    function displayName(data) {
-        console.log(data)
-        $("h1").text(data.name);
-    }
+    $.get("https://api.github.com/users/1digitalnomad", function (res) {
+        console.log(res.name);
+        $("h1").text(res.name);
+    }, 'json')
 });
+
+//or
+
+// $("button").click(function () {
+//     $.get("https://api.github.com/users/1digitalnomad", displayName)
+//     function displayName(data) {
+//         console.log(data)
+//         $("h1").text(data.name);
+//     }
+// });
